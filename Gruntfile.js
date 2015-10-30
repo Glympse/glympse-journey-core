13777a2ff900cb3bbad8dff3ec73c5bf5b34f88b
Jason Keimig <jason@jkeimig.com>
2015-10-30 01:07:43 -0700

    Version update

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
 		distExports: 'dist',
 		moduleIn: 'JourneyCore',
 		moduleOut: 'glympse-journey-core',
-		moduleVersion: '1.5.3',
+		moduleVersion: '1.5.4',
 		buildsDir: 'builds'
       }
     }
