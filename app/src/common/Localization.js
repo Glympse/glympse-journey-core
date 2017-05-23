14ac789ea776ab08e3639ceb22f64ffca2a09acc
Dmitry Radyuk <dradziuk>
2017-05-23 14:45:14 +0300

    FE-115: updated logic to get localized version of strings

--- app/src/common/Localization.js
+++ app/src/common/Localization.js
@@ -8,116 +8,72 @@ define(function (require, exports, module)
 		var lib = require('common/utils');
 
 		var cLocaleEn = 'en';
+		var localesQueue = [];
+		var resultStrings = [];
 
-		var strings;
-		var currLoc;
-		var currLangMarket;
-		var defaultLoc = cLocaleEn;
-
-		var langMarketStrings;
-		var langStrings;
-		var defaultLocStrings;
-		var hardcodedLocStrings;
-
-
-		function _setDefaultLoc(targetDefaultLoc)
+		function _addStrings(stringsObj, locale)
 		{
-			var newDefault = ((strings[targetDefaultLoc] && targetDefaultLoc) || (strings[defaultLoc] && defaultLoc) || (strings[cLocaleEn] && cLocaleEn));
-
-			// Notify if we've switched
-			if (newDefault !== targetDefaultLoc)
+			if (!locale)
 			{
-				console.log('[Localization] loc not found: "' + targetDefaultLoc + '" .. using "' + newDefault + '" instead');
+				return;
 			}
-
-			defaultLoc = newDefault;
-		}
-
-		function _setLoc()
-		{
-			currLoc = currLoc.replace('_', '-').toLowerCase();
-
-			if (currLoc.indexOf('-') > -1)
-			{
-				currLangMarket = currLoc;
-				currLoc = currLangMarket.split('-')[0];
+			var langs = locale.replace('_', '-').toLowerCase().split('-');
+			while (langs.length) {
+				var lang = langs.join('-');
+				var langStrings = stringsObj[lang];
+				if (lang && (localesQueue.indexOf(lang) === -1))
+				{
+					localesQueue.push(lang);
+				}
+				if (langStrings && (resultStrings.indexOf(langStrings)  === -1))
+				{
+					resultStrings.push(langStrings);
+				}
+				langs.pop();
 			}
-
-			langMarketStrings = strings[currLangMarket] || {};
-			langStrings = strings[currLoc] || {};
-
 		}
-
-		///////////////////////////////////////////////////////////////////////////////
-		// PROPERTIES
-		///////////////////////////////////////////////////////////////////////////////
-
-		this.getLoc = function ()
-		{
-			return currLoc;
-		};
-		this.setLoc = function (newLoc)
-		{
-			currLoc = newLoc;
-			_setLoc();
-		};
-		this.getDefaultLoc = function ()
-		{
-			return defaultLoc;
-		};
-		this.setDefaultLoc = function (newDefaultLoc)
-		{
-			_setDefaultLoc(newDefaultLoc);
-		};
-
 
 		///////////////////////////////////////////////////////////////////////////////
 		// PUBLICS
 		///////////////////////////////////////////////////////////////////////////////
 
-		this.defineStrings = function (stringSet)
+		this.defineStrings = function (stringsObj)
 		{
-			strings = stringSet;
-			defaultLoc = _setDefaultLoc(strings.defaultLoc);
-
-			if (!currLoc)
-			{
-				currLoc = lib.getBrowserLoc();
-			}
-
-			//if there are no locales, we don't need to worry about them
-			if (!strings.hasLocales)
-			{
-				currLoc = cLocaleEn;
-			}
-			else
-			//set the locale from the param override
-			if (strings.lang)
-			{
-				currLoc = strings.lang;
-			}
-
-			_setLoc();
-
-			defaultLocStrings = strings[defaultLoc] || {};
-			hardcodedLocStrings = strings[cLocaleEn] || {};
+			// 1. Add strings with lang from urlParams
+			_addStrings(stringsObj, stringsObj.lang);
+			// 2. Add strings for browser's locale
+			_addStrings(stringsObj, lib.getBrowserLoc());
+			// 3. Add strings for defaultLoc from config
+			_addStrings(stringsObj, stringsObj.defaultLoc);
+			// 4. Add strings for default lang (en)
+			_addStrings(stringsObj, cLocaleEn);
 		};
 
 		this.getString = function (stringId)
 		{
-			var currString = langMarketStrings[stringId] || langStrings[stringId] || defaultLocStrings[stringId] || hardcodedLocStrings[stringId];
-			//console.log(stringId + ': ' + currString);
-
-			if (currString === undefined)
+			for (var i = 0, len = resultStrings.length; i < len; i++)
 			{
-				console.log('NOT_FOUND: String not found: ' + stringId);
-				currString = '';
+				var val = resultStrings[i][stringId];
+				if (val)
+				{
+					return val;
+				}
 			}
+			console.warn('NOT_FOUND: String not found: ' + stringId);
+			return '';
+		};
 
-			return currString;
+		// Need this function to get localized version of additional strings (e.g. rescheduleWrapper in `fido` config).
+		// May be need store all strings under `strings` key in config and get rid of this function
+		this.getLocalizedStrings = function(strings) {
+			for (var i = 0, len = localesQueue.length; i < len; i++)
+			{
+				if (strings[localesQueue[i]]) {
+					return strings[localesQueue[i]];
+				}
+			}
+			return {};
 		}
-
 	}
-
 	module.exports = new Localization();
 });
