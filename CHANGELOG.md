d038b0bf3d326fec2d4782737b8b557426335e15
Jason Keimig <jason@glympse.com>
2017-06-22 11:19:31 -0700

    Version 1.7.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 version |date        |notes
 :-------|:-----------|:------
+1.7.2   | 2017/06/22 | Phase.None added
 1.7.1   | 2017/06/17 | Loc bugix
 1.7.0   | 2017/06/17 | Localization class, sync to GA 2.4.0
 1.6.0   | 2017/02/10 | Sync of glympse-adapter 2.0.0
--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         distExports: 'dist',
         moduleIn: 'JourneyCore',
         moduleOut: 'glympse-journey-core',
-        moduleVersion: '1.7.1',
+        moduleVersion: '1.7.2',
         buildsDir: 'builds'
       }
     }
