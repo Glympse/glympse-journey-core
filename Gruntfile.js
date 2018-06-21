b990c7b43fc3088cc150c1234a3bf7426fbf3c3d
iburskyy <iburskyy@exadel.com>
2018-06-21 14:53:55 +0300

    build 1.9.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         distExports: 'dist',
         moduleIn: 'JourneyCore',
         moduleOut: 'glympse-journey-core',
-        moduleVersion: '1.9.0',
+        moduleVersion: '1.9.1',
         buildsDir: 'builds'
       }
     }
